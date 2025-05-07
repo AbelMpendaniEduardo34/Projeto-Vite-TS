919f345a6537c1eeca7729caee98c2a75fbf9203
Abel Mpendani Eduardo <amasolutions.ao@gmail.com>
2025-05-08 00:45:50 +0100

    Subindo alteração atual

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,6 +1,6 @@
 const App = () => {
   return (
-    <div className='w-full h-screen flex items-center justify-center'>
+    <div className='w-full h-screen flex items-center justify-center bg-black'>
       <h1>Olá mundo</h1>
     </div>
   )
