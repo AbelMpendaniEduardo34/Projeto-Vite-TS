ea3bd6c689099dae383fc52ce6dba17aa5a0fdbc
Abel Mpendani Eduardo <amasolutions.ao@gmail.com>
2025-05-08 00:27:46 +0100

    Segundo commit

---
+++ src/routes/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-full h-screen flex items-center justify-center'>
+      <h1>Olá mundo</h1>
+    </div>
+  )
+}
+
+export default App
+
